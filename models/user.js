3b10e02032fd31548452616fc1519d0698d3d1cd
Andrew Fisher <Andrew Fisher>
2016-07-17 15:31:49 -0700

    Working on getting jQuery mobile working on mobile, does work on browser

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-var user = function(name, password, teams, friends) {
-	this.name = name;
-	this.password = password;
-	this.teams = teams;
-	this.friends = friends;
+var user;
+user = function (name, password, teams, friends) {
+    this.name = name;
+    this.password = password;
+    this.teams = teams;
+    this.friends = friends;
 };
