99eb571d16887228bdfebc68ce9a56ea288dcceb
Andrew Fisher <Andrew Fisher>
2016-07-22 17:45:48 -0700

    some more functionality on the panel

--- models/team.js
+++ models/team.js
@@ -1,7 +1,6 @@
-var team;
-team = function (name, id, main) {
+function Team(name, id, main) {
     this.name = name;
     this.players = [];
     this.id = id;
     this.main = main;
-};
+}
--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
-/**
- * @param user
- * @param user.teams
- */
 
-var user;
-user = function (name, password) {
+
+
+function User (name, password, numTeams) {
     this.name = name;
+    this.numTeams = numTeams;
     this.password = password;
     this.teams = [];
     this.friends = [];
+}
+User.prototype.addTeam = function(team){
+    this.teams.push(team);
+    this.numTeams += 1;
+};
+User.prototype.hasTeams = function(){
+   return this.numTeams;
 };
